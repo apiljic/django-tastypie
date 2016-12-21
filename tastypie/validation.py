17ce494659c4466873c66777da929823426b8927
Alen Piljic <piljic@gmail.com>
2016-12-21 20:09:54 +0100

    Validation.py updated with changes from Aljosa's fork
    This fixes the issue django-tastypie used to have (and still has?) when creating objects with related fields.

--- tastypie/validation.py
+++ tastypie/validation.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from django.core.exceptions import ImproperlyConfigured
 from django.forms import ModelForm
 from django.forms.models import model_to_dict
-
+from django.db.models.fields.related import RelatedField
 
 class Validation(object):
     """
@@ -15,7 +15,6 @@ class Validation(object):
         """
         Performs a check on the data within the bundle (and optionally the
         request) to ensure it is valid.
-
         Should return a dictionary of error messages. If the dictionary has
         zero items, the data is considered valid. If there are errors, keys
         in the dictionary should be field names and the values should be a list
@@ -27,24 +26,28 @@ class Validation(object):
 class FormValidation(Validation):
     """
     A validation class that uses a Django ``Form`` to validate the data.
-
     This class **DOES NOT** alter the data sent, only verifies it. If you
     want to alter the data, please use the ``CleanedDataFormValidation`` class
     instead.
-
     This class requires a ``form_class`` argument, which should be a Django
     ``Form`` (or ``ModelForm``, though ``save`` will never be called) class.
     This form will be used to validate the data in ``bundle.data``.
     """
     def __init__(self, **kwargs):
-        if 'form_class' not in kwargs:
-            raise ImproperlyConfigured(
-                "You must provide a 'form_class' to 'FormValidation' classes.")
+        if not 'form_class' in kwargs:
+            raise ImproperlyConfigured("You must provide a 'form_class' to 'FormValidation' classes.")
 
         self.form_class = kwargs.pop('form_class')
         super(FormValidation, self).__init__(**kwargs)
 
     def form_args(self, bundle):
+        '''
+        Use the model data to generate the form arguments to be used for
+        validation.  In the case of fields that had to be hydrated (such as
+        FK relationships), be sure to use the hydrated value (comes from 
+        model_to_dict()) rather than the value in bundle.data, since the latter
+        would likely not validate as the form won't expect a URI.
+        '''
         data = bundle.data
 
         # Ensure we get a bound Form, regardless of the state of the bundle.
@@ -52,20 +55,27 @@ class FormValidation(Validation):
             data = {}
 
         kwargs = {'data': {}}
-
         if hasattr(bundle.obj, 'pk'):
             if issubclass(self.form_class, ModelForm):
                 kwargs['instance'] = bundle.obj
 
             kwargs['data'] = model_to_dict(bundle.obj)
-
-        kwargs['data'].update(data)
+            # iterate over the fields in the object and find those that are
+            # related fields - FK, M2M, O2M, etc.  In those cases, we need
+            # to *not* use the data in the bundle, since it is a URI to a
+            # resource.  Instead, use the output of model_to_dict for 
+            # validation, since that is already properly hydrated.
+            for field in bundle.obj._meta.fields:
+                if field.name in bundle.data: 
+                    if not isinstance(field, RelatedField):
+                        kwargs['data'][field.name]=bundle.data[field.name]
+        else:
+            kwargs['data'].update(data)
         return kwargs
 
     def is_valid(self, bundle, request=None):
         """
         Performs a check on ``bundle.data``to ensure it is valid.
-
         If the form is valid, an empty list (all valid) will be returned. If
         not, a list of errors will be returned.
         """
@@ -83,9 +93,7 @@ class FormValidation(Validation):
 class CleanedDataFormValidation(FormValidation):
     """
     A validation class that uses a Django ``Form`` to validate the data.
-
     This class **ALTERS** data sent by the user!!!
-
     This class requires a ``form_class`` argument, which should be a Django
     ``Form`` (or ``ModelForm``, though ``save`` will never be called) class.
     This form will be used to validate the data in ``bundle.data``.
@@ -94,7 +102,6 @@ class CleanedDataFormValidation(FormValidation):
         """
         Checks ``bundle.data``to ensure it is valid & replaces it with the
         cleaned results.
-
         If the form is valid, an empty list (all valid) will be returned. If
         not, a list of errors will be returned.
         """
